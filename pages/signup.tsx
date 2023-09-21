7afd7d93c2c204fba32dd9112744b8ed6afbf57e
lutitech <lutor4148@gmail.com>
2023-09-21 04:08:43 +0100

    latest

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import RegisterForm from '@/components/RegisterForm';
+import RegisterForm from '../components/RegisterForm';
 
 const Home: React.FC = () => {
   return (
